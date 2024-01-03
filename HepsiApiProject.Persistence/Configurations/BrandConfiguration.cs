42017be94f6806883047cb952cf8a9a04a728603
UmitBalikci <u.balikci.bm@gmail.com>
2024-01-03 11:44:42 +0300

    Add configuration classes

---
+++ HepsiApiProject.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using HepsiApiProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(255);
+        }
+    }
+}
---
+++ HepsiApiProject.Persistence/Configurations/DetailConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using HepsiApiProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Persistence.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Detail>
+    {
+        public void Configure(EntityTypeBuilder<Detail> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
