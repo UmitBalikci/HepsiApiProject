c30c7d30a3f65488c15918d51489206dfd90de3c
UmitBalikci <u.balikci.bm@gmail.com>
2024-02-09 14:05:05 +0300

    Add BaseRules

---
+++ Core/HepsiApiProject.Application/Features/Products/Exceptions/ProductTitleException.cs
@@ -0,0 +1,17 @@
+﻿using HepsiApiProject.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Application.Features.Products.Exceptions
+{
+    public class ProductTitleException : BaseExceptions
+    {
+        public ProductTitleException() : base("Product Title exist!")
+        {
+            
+        }
+    }
+}
---
+++ Core/HepsiApiProject.Application/Features/Products/Rules/ProductRules.cs
@@ -0,0 +1,14 @@
+﻿using HepsiApiProject.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Application.Features.Products.Rules
+{
+    public class ProductRules : BaseRules
+    {
+        public 
+    }
+}
