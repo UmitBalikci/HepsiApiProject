dad3fdcb814c7357c7d3d8b7af6b47232d548d8a
UmitBalikci <u.balikci.bm@gmail.com>
2024-03-07 17:44:24 +0300

    Add revoke and revokeAll

---
+++ Core/HepsiApiProject.Application/Features/Auth/Commands/Revoke/RevokeCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using HepsiApiProject.Application.Bases;
+using HepsiApiProject.Application.Features.Auth.Rules;
+using HepsiApiProject.Application.Interfaces.AutoMapper;
+using HepsiApiProject.Application.Interfaces.UnitOfWorks;
+using HepsiApiProject.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Application.Features.Auth.Commands.Revoke
+{
+    public class RevokeCommandHandler : BaseHandler, IRequestHandler<RevokeCommandRequest, Unit>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly AuthRules _authRules;
+
+        public RevokeCommandHandler(UserManager<User> userManager, AuthRules authRules, IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            _userManager = userManager;
+            _authRules = authRules;
+        }
+
+        public async Task<Unit> Handle(RevokeCommandRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+
+            await _authRules.EmailAddressShouldBeValid(user);
+
+            user.RefreshToken = null;
+
+            await _userManager.UpdateAsync(user);
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Core/HepsiApiProject.Application/Features/Auth/Exceptions/EmailAddressShouldBeValidException.cs
@@ -0,0 +1,17 @@
+﻿using HepsiApiProject.Application.Bases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApiProject.Application.Features.Auth.Exceptions
+{
+    public class EmailAddressShouldBeValidException : BaseExceptions
+    {
+        public EmailAddressShouldBeValidException() : base("This email not found!")
+        {
+            
+        }
+    }
+}
